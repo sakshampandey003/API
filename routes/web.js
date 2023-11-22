0c7d3766d372f2f8469c348b944d0c62637736a4
rauakmpct <rkloveak0118@gmail.com>
2023-11-22 08:51:35 +0530

    d

--- routes/web.js
+++ routes/web.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const UserController = require('../controllers/UserController')
+const CategoryController = require('../controllers/CategoryController')
+const ProductController = require('../controllers/ProductController')
 const router = express.Router()
 
-// usercontroller 
-router.get('/getalluser',UserController.getalluser)
-router.post('/userinsert',UserController.userinsert)
-router.post('/verifylogin',UserController.verifylogin)
+// UserController 
+router.get('/getalluser', UserController.getalluser)
+router.post('/userinsert', UserController.userinsert)
+router.post('/verifylogin', UserController.verifylogin)
+
+//CategoryController
+router.post('/categoryinsert', CategoryController.categoryinsert)
+router.get('/categorydisplay', CategoryController.categorydisplay)
+router.get('/categoryview', CategoryController.categoryview)
+router.post('/categoryupdate', CategoryController.categoryupdate)
+router.get('/categorydelete', CategoryController.categorydelete)
+
+//ProductController
+router.post('/createproduct', ProductController.createproduct)
+router.get('/getallproduct', ProductController.getallproduct)
+router.get('/getallproductdetail', ProductController.getallproductdetail)
+router.post('/updateproduct', ProductController.updateproduct)
+router.get('/deleteproduct', ProductController.deleteproduct)
 
 
 
